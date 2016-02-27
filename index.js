d2ee6c1227a207508ac36a93135017201c05cc2c
Marco Piraccini <marco.piraccini@gmail.com>
2016-02-27 22:35:21 +0100

    Firt workting version rking version

--- index.js
+++ index.js
@@ -18,74 +18,132 @@ var fs = require('fs')
 var split2 = require('split2')
 var eos = require('end-of-stream')
 
-var knownTypes = ['C', 'AT', 'HID', 'R', 'BIAS', 'PHY', 'HUT', 'L', 'HCC', 'VT']
-
-var elements = []
-var currentPath = []
-var currentLevel = 0
-// When a tab is met, the number of tab is counted.
-// if numberOfTabs = currentLevel + 1 =>
-//    currentLevel++
-//    currentPath.push(key)
-
-var usbInfoStream = fs.createReadStream('./ids/usb.ids')
-  .pipe(split2())
-  .on('data', function (line) {
-    if ((line.trim().startsWith('#')) || (!line.trim())) return // Skip comments and empty lines
-
-    var lineArr = line.split(' ')
-    // Get the current type (is level === 0)
-    // C -> Class (List of known device classes, subclasses and protocols)
-    // AT -> List of Audio Class Terminal Types
-    // HID -> List of HID Descriptor Types
-    // R -> List of HID Descriptor Item Types
-    // BIAS -> List of Physical Descriptor Bias Types
-    // PHY -> List of Physical Descriptor Item Types
-    // HUT -> List of HID Usages
-    // L -> List lf languages
-    // HCC -> HID Descriptor bCountryCode
-    // VT -> List of Video Class Terminal Types
-    var type
-    if (knownTypes.indexOf(lineArr[0].trim()) !== -1) {
-      type = lineArr.shift()
-    }
-
-    var key = lineArr.shift().trim()
-    var value
-
-    if (lineArr) {
-      value = lineArr.join(' ')
-    }
-
-    var numberOfTabs = countTabs(line) // calculate the "level" form the number of the used tabs
-
-    if (numberOfTabs === 0) {
-      currentPath = [key]
-      currentLevel = 0
-    }
-    // Increment the level
-    if (numberOfTabs === (currentLevel + 1)) {
-      currentLevel++
-      currentPath.push(key)
-    }
-
-    var element = {
-      key: key,
-      value: value,
-      path: currentPath.slice(0)
-    }
-    if (type) element.type = type
-
-    elements.push(element)
-  })
-
-eos(usbInfoStream, function (err) {
-  if (err) return console.log('stream had an error or closed early')
-})
-
-function countTabs (line) {
+var countTabs = function (line) {
   return line.split('').reduce((prev, car) => {
     if (car === '\t') return ++prev
     return prev
   }, 0)
+}
+
+/**
+ * Get the UDB info, with cache
+ */
+var getUsbInfoCached = function (cb) {
+  var usbInfo
+  return function (cb) {
+    if (usbInfo) return cb(null, usbInfo)
+
+    var knownTypes = ['C', 'AT', 'HID', 'R', 'BIAS', 'PHY', 'HUT', 'L', 'HCC', 'VT']
+
+    var elements = []
+    var currentPath = []
+    var currentLevel = 0
+    var currentType = 'device'
+    // When a tab is met, the number of tab is counted.
+    // if numberOfTabs = currentLevel + 1 =>
+    //    currentLevel++
+    //    currentPath.push(key)
+    // (All that works because the list is ordered)
+
+    var usbInfoStream = fs.createReadStream('./ids/usb.ids')
+      .pipe(split2())
+      .on('data', function (line) {
+        if ((line.trim().startsWith('#')) || (!line.trim())) return // Skip comments and empty lines
+
+        var lineArr = line.split(' ')
+        // Get the current type (is level === 0)
+        // C -> Class (List of known device classes, subclasses and protocols)
+        // AT -> List of Audio Class Terminal Types
+        // HID -> List of HID Descriptor Types
+        // R -> List of HID Descriptor Item Types
+        // BIAS -> List of Physical Descriptor Bias Types
+        // PHY -> List of Physical Descriptor Item Types
+        // HUT -> List of HID Usages
+        // L -> List lf languages
+        // HCC -> HID Descriptor bCountryCode
+        // VT -> List of Video Class Terminal Types
+        if (knownTypes.indexOf(lineArr[0].trim()) !== -1) {
+          currentType = lineArr.shift()
+        }
+
+        var key = lineArr.shift().trim()
+        var value
+
+        if (lineArr) {
+          value = lineArr.join(' ').trim()
+        }
+
+        var numberOfTabs = countTabs(line) // calculate the "level" form the number of the used tabs
+
+        if (numberOfTabs === 0) {
+          currentPath = [key]
+          currentLevel = 0
+        }
+        // Increment the level
+        if (numberOfTabs === (currentLevel + 1)) {
+          currentLevel++
+          currentPath.push(key)
+        } else {
+          // Same level, changing path
+          currentPath[currentPath.length - 1] = key
+        }
+
+        var element = {
+          key: key,
+          value: value,
+          path: currentPath.slice(0)
+        }
+
+        if (!elements[currentType]) elements[currentType] = []
+        elements[currentType].push(element)
+      })
+
+    eos(usbInfoStream, function (err) {
+      if (err) return cb('stream had an error or closed early') // TODO: better mgmt
+      usbInfo = elements
+      cb(null, usbInfo)
+    })
+  }
+}
+
+var getUsbInfo = getUsbInfoCached()
+
+var getDevice = function (vendorId, deviceId, cb) {
+  getUsbInfo((err, info) => {
+    if (err) return cb(err)
+    var found = info.device.reduce((prev, item) => {
+      if ((item.path[0] === vendorId) && (item.path[1] === deviceId)) {
+        prev = item
+      }
+      return prev
+    }, null)
+    if (!found) return cb()
+    cb(null, {
+      vendorId: found.path[0],
+      deviceId: found.path[1],
+      description: found.value
+    })
+  })
+}
+
+var getVendor = function (vendorId, cb) {
+  getUsbInfo((err, info) => {
+    if (err) return cb(err)
+    var found = info.device.reduce((prev, item) => {
+      if (item.path[0] === vendorId) {
+        prev = item
+      }
+      return prev
+    }, null)
+    if (!found) return cb()
+    return cb(null, {
+      vendorId: found.path[0],
+      description: found.value
+    })
+  })
+}
+
+module.exports = {
+  getDevice: getDevice,
+  getVendor: getVendor
 }
--- test/test.js
+++ test/test.js
@@ -1,8 +1,69 @@
 'use strict'
 
 var test = require('tape')
-require('..')
+var usbinfo = require('..')
 
-test('TEST - TODO', function (t) {
-  t.end()
+test('Test Get Device - existing vendor and device - 1', function (t) {
+  var expected = {
+    vendorId: '0001',
+    deviceId: '7778',
+    description: 'Counterfeit flash drive [Kingston]'
+  }
+
+  usbinfo.getDevice('0001', '7778', (err, device) => {
+    if (err) t.fail(err)
+    t.deepEqual(device, expected)
+    t.end()
+  })
+})
+
+test('Test Get Device - existing vendor and device - 2', function (t) {
+  var expected = {
+    vendorId: '03eb',
+    deviceId: '2044',
+    description: 'LUFA CDC Demo Application'
+  }
+
+  usbinfo.getDevice('03eb', '2044', (err, device) => {
+    if (err) t.fail(err)
+    t.deepEqual(device, expected)
+    t.end()
+  })
+})
+
+test('Test Get Device - NOT existing vendor and device', function (t) {
+  usbinfo.getDevice('xxx', 'yyyy', (err, device) => {
+    if (err) t.fail(err)
+    t.notOk(device)
+    t.end()
+  })
+})
+
+test('Test Get Device - existing vendor but not device', function (t) {
+  usbinfo.getDevice('0001', 'yyyy', (err, device) => {
+    if (err) t.fail(err)
+    t.notOk(device)
+    t.end()
+  })
+})
+
+test('Test Get Vendor - existing vendor with no devices', function (t) {
+  var expected = {
+    vendorId: '03ec',
+    description: 'Iwatsu America, Inc.'
+  }
+
+  usbinfo.getVendor('03ec', (err, vendor) => {
+    if (err) t.fail(err)
+    t.deepEqual(vendor, expected)
+    t.end()
+  })
+})
+
+test('Test Get Vendor - NON existing vendor', function (t) {
+  usbinfo.getVendor('xxxxx', (err, device) => {
+    if (err) t.fail(err)
+    t.notOk(device)
+    t.end()
+  })
 })
