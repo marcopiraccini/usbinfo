06f70f1bdb2ad19bf13ee4919e5c68333c4831b5
Peter Kazazes <peter@peterk.co>
2019-01-03 22:07:40 -0500

    rewrite in typescript

---
+++ dist/usbinfo.d.ts
@@ -0,0 +1,3 @@
+export declare const getVendor: (vendorId: string) => Promise<{}>;
+export declare const getProduct: (vendorId: any, productId: any) => Promise<{}>;
+//# sourceMappingURL=usbinfo.d.ts.map
